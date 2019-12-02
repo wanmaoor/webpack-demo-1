4603d9a6734940505b42da6246a92005df9d5173
wanmaoor <wanmao1284879375@outlook.com>
2019-12-02 22:52:14 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const {resolve} = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: resolve(__dirname, "dist") //__dirname 代表当前目录，path.resolve()将一段路径解析为绝对路径
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
