60bbcff35d5e11edfaadc85acfd7e217660a6f20
wanmaoor <wanmao1284879375@outlook.com>
2019-12-06 14:16:32 +0800

    lazy load

--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import "./assets/2.css";
 import "./assets/3.scss";
 import "./assets/3.less";
 import "./assets/3.styl";
-import png from './assets/3.png'
+import png from "./assets/3.png";
 
-function component() { 
+function component() {
   const element = document.createElement("div");
-   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  const button = document.createElement("button");
+  button.innerText = "点击懒加载";
+  button.onclick = () => {
+    const promise = import("./assets/lazy");
+    promise.then(module => {
+      module.default();
+    });
+  };
+  element.appendChild(button);
   element.classList.add("hello");
-  const img = new Image()
-  img.src = png
-  element.appendChild(img)
-  return element
+  const img = new Image();
+  img.src = png;
+  element.appendChild(img);
+  return element;
 }
-document.body.appendChild(component())
+document.body.appendChild(component());
 
 console.log(123);
